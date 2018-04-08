25242d764174b34bba58353768354f26be88fa1c
oninbo <oninbo@gmail.com>
2018-04-08 15:23:07 +0400

    game object

---
+++ Assets/scripts/Handlers.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Handlers : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    delegate void Handler();
+    Handler GetHandler(GameObject obj1, GameObject obj2){
+        return delegate ()
+        {
+            Debug.Log("test");
+        };
+    }
+}
